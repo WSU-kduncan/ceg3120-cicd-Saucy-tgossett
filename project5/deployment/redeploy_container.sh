4f29b84ba106d6b46af51001cace06f7951fa398
Saucy-tgossett <156851308+Saucy-tgossett@users.noreply.github.com>
2025-05-01 16:16:09 -0400

    updating redeployment script

--- project5/deployment/redeploy_container.sh
+++ project5/deployment/redeploy_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 IMAGE="saucydorito/gossett-ceg3120:latest"
-CONTAINER_NAME="angular"
+CONTAINER_NAME="angular-site"
 
 docker pull $IMAGE
 docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME
-docker run -d -p 4200:4200 --name $CONTAINER_NAME $IMAGE
+docker run -it -d -p 4200:4200 --name $CONTAINER_NAME $IMAGE 
